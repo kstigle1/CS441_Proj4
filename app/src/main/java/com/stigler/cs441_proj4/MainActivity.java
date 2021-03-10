c9d60bd38fe2bbff5924a75cdb328668f2b2ecec
stigglesworth3 <Kyle@Stigler.com>
2021-03-10 11:09:17 -0500

    added EditText and button to have something for now

--- app/src/main/java/com/stigler/cs441_proj4/MainActivity.java
+++ app/src/main/java/com/stigler/cs441_proj4/MainActivity.java
@@ -3,12 +3,28 @@ package com.stigler.cs441_proj4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    TextView inputLabel;
+    EditText inputBox;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputLabel = (TextView) findViewById(R.id.labelText);
+        inputBox = (EditText) findViewById(R.id.inputBox);
+    }
+
+    public void buttonPress( View view)
+    {
+        inputLabel.setText(inputBox.getText().toString());
+        inputBox.setText("");
     }
 }
