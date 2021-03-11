7369b2dbca0fa78ed109f97c48238b80c6a9fa9b
stigglesworth3 <Kyle@Stigler.com>
2021-03-10 20:50:02 -0500

    add lines to table with button press, not exactly how I want

--- app/src/main/java/com/stigler/cs441_proj4/MainActivity.java
+++ app/src/main/java/com/stigler/cs441_proj4/MainActivity.java
@@ -5,12 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
 {
     TextView inputLabel;
     EditText inputBox;
+    TableLayout table;
+    ArrayList<TableRow> rowList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,11 +26,24 @@ public class MainActivity extends AppCompatActivity
 
         inputLabel = (TextView) findViewById(R.id.labelText);
         inputBox = (EditText) findViewById(R.id.inputBox);
+        table = (TableLayout) findViewById(R.id.theTable);
+        rowList = new ArrayList<>();
     }
 
-    public void buttonPress( View view)
+    public void addButton( View view)
     {
-        inputLabel.setText(inputBox.getText().toString());
-        inputBox.setText("");
+        /*inputLabel.setText(inputBox.getText().toString());
+        inputBox.setText("");*/
+        TableRow tRow = new TableRow(this);
+        TextView words = new TextView(this);
+        words.setText("Please work");
+        tRow.addView(words, 0);
+        table.addView(tRow);
+        rowList.add(tRow);
+    }
+
+    public void removeButton(View view)
+    {
+        //
     }
 }
